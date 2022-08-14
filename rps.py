f7279e1ad659218dab6d1b8fef6c59f812d5a0bc
Kaizer-Rai <108696304+Kaizer-Rai@users.noreply.github.com>
2022-08-13 21:25:38 -0500

    Create rps.py

---
+++ rps.py
@@ -0,0 +1,99 @@
+import random
+import time
+#Variables
+user_sel = None
+moves = ('rock', 'paper', 'scissors')
+opp_sel = None
+game = 0
+wins = 0
+losses = 0
+#Loop
+print('welcome to rock, paper, scissors...\n')
+while True:
+#Moves
+    user_sel = input(f'[1]rock \n[2]paper \n[3]scissors \n[input]:')
+    opp_sel = random.choice(moves)
+#Rock
+    if user_sel == '1' and opp_sel == 'rock':
+        time.sleep(1)
+        print('\nopponent selects rock... \nDRAW! \n')
+        time.sleep(1)
+        game = game + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+
+    if user_sel == '1' and opp_sel == 'paper':
+        time.sleep(1)
+        print('\nopponent selects paper... \nYOU LOSE! \n')
+        time.sleep(1)
+        game = game + 1
+        losses = losses + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+
+    if user_sel == '1' and opp_sel == 'scissors':
+        time.sleep(1)
+        print('\nopponent selects scissors... \nYOU WIN! \n')
+        time.sleep(1)
+        game = game + 1
+        wins = wins + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+#Paper
+    if user_sel == '2' and opp_sel == 'rock':
+        time.sleep(1)
+        print('\nopponent selects rock... \nYOU WIN! \n')
+        time.sleep(1)
+        game = game + 1
+        wins = wins + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+
+    if user_sel == '2' and opp_sel == 'paper':
+        time.sleep(1)
+        print('\nopponent selects paper... \nDRAW! \n')
+        time.sleep(1)
+        game = game + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+
+    if user_sel == '2' and opp_sel == 'scissors':
+        time.sleep(1)
+        print('\nopponent selects scissors... \nYOU LOSE! \n')
+        time.sleep(1)
+        game = game + 1
+        losses = losses + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+#Scissors
+    if user_sel == '3' and opp_sel == 'rock':
+        time.sleep(1)
+        print('\nopponent selects rock... \nYOU LOSE! \n')
+        time.sleep(1)
+        game = game + 1
+        losses = losses + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+
+    if user_sel == '3' and opp_sel == 'paper':
+        time.sleep(1)
+        print('\nopponent selects paper... \nYOU WIN! \n')
+        time.sleep(1)
+        game = game + 1
+        wins = wins + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+
+    if user_sel == '3' and opp_sel == 'scissors':
+        time.sleep(1)
+        print('\nopponent selects scissors... \nDRAW! \n')
+        time.sleep(1)
+        game = game + 1
+        print('---Score---\n[game]:', game, '\n[wins]:', wins, '\n[losses]:', losses, '\n')
+        time.sleep(1)
+#Quit
+    if user_sel == 'quit': quit()
+#Input Error
+    if not user_sel == '1' or '2' or '3':
+        print('\ninvalid input, try again...\n')
+        continue
